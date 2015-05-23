8a7cd453fb96b93b115f1261a3e78520a8c0a00a
Julien Fouilhé <julien.fouilhe@gmail.com>
2015-05-23 23:05:20 +1000

    Allow everyone

--- config/express.js
+++ config/express.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
